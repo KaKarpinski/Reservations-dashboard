67e996ed4f70a5aeed616bb31b72c1dd9186bef4
kamil-imovum <kamil@imovum@com>
2025-03-18 20:12:26 +0100

    [RD-012] Implement error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,26 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import ReservationBoardWrapper from "./pages/ReservationsDashboard/ReservationBoardWrapper";
 import AddReservation from "./pages/AddReservation/AddReservation";
 import EditReservationWrapper from "./pages/EditReservation/EditReservationWrapper";
+import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
     <div className="app-container">
       <BrowserRouter>
         <Header />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<ReservationBoardWrapper />} />
-            <Route path="/add" element={<AddReservation />} />
-            <Route path="/edit/:id" element={<EditReservationWrapper />} />
-          </Routes>
-        </main>
+        <ErrorBoundary
+          fallback={
+            <div className="error-fallback">⚠️ Something went wrong</div>
+          }
+        >
+          <main className="main-content">
+            <Routes>
+              <Route path="/" element={<ReservationBoardWrapper />} />
+              <Route path="/add" element={<AddReservation />} />
+              <Route path="/edit/:id" element={<EditReservationWrapper />} />
+            </Routes>
+          </main>
+        </ErrorBoundary>
       </BrowserRouter>
     </div>
   );
